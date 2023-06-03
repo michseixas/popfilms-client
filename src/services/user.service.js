f899191cb4fb51fce5b57ab906bb44f640f2ff38
Erika Balog <enbalog@gmail.com>
2023-06-04 00:07:02 +0200

    Created user service with get/update/delete/updateImage functions for UserProfilePage

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,12 +3,27 @@ let baseUrl = 'http://localhost:5005/user';
 
 
 //this function will be called on the front end on the page that we need. In this case, the user profile
-const getUserInfo = () => {
+
+//get user info by Id
+const getUserInfo = (userId) => {
     return axios.get(baseUrl + '/:userId')
-    
+};
+
+//update user info by Id
+const updateUserInfo= (userId, newData) => {
+    return axios.post(baseUrl + '/update', newData)
+};
+
+//delete user by Id
+const deleteUser= (userId) => {
+    return axios.post(baseUrl + '/delete')
+};
+
+//update user image by Id
+const updateImage= (userId, newImage) => {
+    return axios.post(baseUrl + '/updateImage', {image: newImage})
 }
 
 
 
-
-export {getUserInfo};
+export {getUserInfo, updateUserInfo, deleteUser, updateImage};
