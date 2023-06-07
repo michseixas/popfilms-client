de5b4c76e4c64eb0deb57aff63564d3fe4eb9066
Mich Seixas <mcseixas@gmail.com>
2023-06-07 21:26:37 +0200

    applied conditional rendering to component (filter input) depending on the page the user visits.

--- src/components/FilterForm.jsx
+++ src/components/FilterForm.jsx
@@ -1,22 +1,31 @@
 import React, { useContext, useEffect } from "react";
 import { filterContext } from "../contexts/filter.context";
+import { useLocation } from 'react-router-dom';
 
 function FilterForm() {
   const { handleInputChange, handleInputBlur, text } = useContext(filterContext);
   useEffect(() => {
   }, [text]);
 
-  return (
-    <div>
-      <input
-        type="text" //reflects the current value stored in text
-        value={text}
-        onChange={handleInputChange} //the user types into the input field and triggers OnChange, it then calls the handleInputChange to update the text state and perform the filtering
-        onBlur={handleInputBlur}
-        placeholder="Filter movies..."
-      />
-    </div>
-  );
+  const location = useLocation();
+  const route = location.pathname;
+
+  if (route === "/top250" ||route === "/mostpopular" ||route === "/theater" ||route === "/comingsoon") {
+    return (
+      <div>
+        <input
+          type="text" //reflects the current value stored in text
+          value={text}
+          onChange={handleInputChange} //the user types into the input field and triggers OnChange, it then calls the handleInputChange to update the text state and perform the filtering
+          onBlur={handleInputBlur}
+          placeholder="Filter movies..."
+        />
+      </div>
+    );
+  }
+
+  return null;
+
 }
 
 export default FilterForm;
