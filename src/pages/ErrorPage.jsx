fdbe29b5ff699d774f5de5922a3ef2c746aebb68
Mich Seixas <mcseixas@gmail.com>
2023-06-24 22:34:14 +0200

    added gif to error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function ErrorPage() {
   return (
-    <div className="bg-black text-white">ErrorPage</div>
+    <div className='errorContainer'>
+    <div className="bg-black text-white"><br/>Oh-Oh! Page not found!<br/><br/><iframe src="https://giphy.com/embed/6uGhT1O4sxpi8" width="480" height="240" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p></div>
+    </div>
   )
 }
 
